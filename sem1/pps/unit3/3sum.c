dd553ef41df7062954f3960932aa69f5da187ed4
manohar saini <theogtanishh@gmail.com>
2026-08-15 17:45:10 +0530

    fix bug in 3sum

--- sem1/pps/unit3/3sum.c
+++ sem1/pps/unit3/3sum.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int main(){    
+int main(){
     int o , t , h , sum , threesum;
     printf("Enter Three Digit Number: ");
     scanf("%d", &threesum);
     h = threesum / 100;
-    t = (threesum / 100 ) % 10;
+    t = (threesum / 10 ) % 10;
     o = threesum % 10;
     sum = t + h + o;
     printf("Total Sum is %d", sum);
